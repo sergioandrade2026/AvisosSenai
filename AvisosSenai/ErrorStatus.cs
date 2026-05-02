cc7db6dbc85c5dd3ca62cf6aeb3426b7dc1474e0
semra <sergioandrade2045@gmail.com>
2026-05-02 02:03:15 -0300

    Revert "hhhhhhhhhhhhhhh"
    This reverts commit ad63aca7933293ab27b3872d538c7bb17f8ca0e1.

--- AvisosSenai/ErrorStatus.cs
+++ AvisosSenai/ErrorStatus.cs
@@ -3,5 +3,5 @@ using System.Text;
 
 namespace AvisosSenai
 {
-    public enum ErrorStatus { None,FileError,MemoryError,ServerError,ConectionError,hhhhhhhhhh}
+    public enum ErrorStatus { None,FileError,MemoryError,ServerError,ConectionError}
 } 
