a61018c9f8ecc75dee2c268bc44c8fc9a3fd924a
semra <sergioandrade2045@gmail.com>
2026-05-02 02:00:06 -0300

    Revert "lllllllllllll"
    This reverts commit ce822410659d59505724455132d81b8270aa82ce.

--- AvisosSenai/ErrorStatus.cs
+++ AvisosSenai/ErrorStatus.cs
@@ -3,5 +3,5 @@ using System.Text;
 
 namespace AvisosSenai
 {
-    public enum ErrorStatus { None,FileError,MemoryError,ServerError,ConectionErrorllllllllllllll}
+    public enum ErrorStatus { None,FileError,MemoryError,ServerError,ConectionError}
 } 
