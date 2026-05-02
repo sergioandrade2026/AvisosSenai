6532d21718d9e8024fe7bb68a8ceb1b9236edb39
semra <sergioandrade2045@gmail.com>
2026-05-02 01:59:17 -0300

    Revert "dd"
    This reverts commit 7ccae20f3d72bfc8167f140999e616c7fa49fc24.

--- AvisosSenai/ErrorStatus.cs
+++ AvisosSenai/ErrorStatus.cs
@@ -3,5 +3,5 @@ using System.Text;
 
 namespace AvisosSenai
 {
-    public enum ErrorStatus { None,FileError,MemoryError,ServerError,ConectionErrorddd}
+    public enum ErrorStatus { None,FileError,MemoryError,ServerError,ConectionError}
 } 
