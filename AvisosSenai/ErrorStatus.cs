95b9d250b022b3d1818c80219cc84ce43a201397
semra <sergioandrade2045@gmail.com>
2026-05-02 02:08:06 -0300

    Revert "aaaa"
    This reverts commit 6e5b8ad2cb205fd393566582119f69710c721472.

--- AvisosSenai/ErrorStatus.cs
+++ AvisosSenai/ErrorStatus.cs
@@ -3,5 +3,5 @@ using System.Text;
 
 namespace AvisosSenai
 {
-    public enum ErrorStatus { None,FileError,MemoryError,ServerError,ConectionError,aaaa}
+    public enum ErrorStatus { None,FileError,MemoryError,ServerError,ConectionError}
 } 
