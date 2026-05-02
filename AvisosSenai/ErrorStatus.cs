7413b7d86d67698dcb14e44ac5cc753ddd2bbf08
semra <sergioandrade2045@gmail.com>
2026-05-02 01:53:57 -0300

    kkkkkkkk

--- AvisosSenai/ErrorStatus.cs
+++ AvisosSenai/ErrorStatus.cs
@@ -3,5 +3,5 @@ using System.Text;
 
 namespace AvisosSenai
 {
-    public enum ErrorStatus { None,FileError,MemoryError,ServerError,ConectionErrorcccc}
+    public enum ErrorStatus { None,FileError,MemoryError,ServerError,ConectionErrorkkkkk}
 } 
