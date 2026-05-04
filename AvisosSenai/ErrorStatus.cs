2c08affec89cb0aaaad8e2b09b95c88992259252
semra <sergioandrade2045@gmail.com>
2026-05-04 12:31:47 -0300

    beta 0.5

--- AvisosSenai/ErrorStatus.cs
+++ AvisosSenai/ErrorStatus.cs
@@ -3,6 +3,8 @@ using System.Text;
 
 namespace AvisosSenai
 {
-    // new branch commit
+
     public enum ErrorStatus { None,FileError,MemoryError,ServerError,ConectionError}
+
+  
 } 
--- AvisosSenai/WebServer.cs
+++ AvisosSenai/WebServer.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Net;
-using System.Net.Sockets;
+using System.Text;
 using System.Threading;
 
 namespace AvisosSenai
@@ -9,8 +9,8 @@ namespace AvisosSenai
     public class WebServer
     {
         private HttpListener _listener;
-     
         private readonly string _webRoot = @"I:\website\";
+        private readonly string _dbPath = @"I:\website\db.json";
 
         public void Start(int port = 80)
         {
@@ -24,24 +24,19 @@ namespace AvisosSenai
             while (true)
             {
                 var context = _listener.GetContext();
+                var request = context.Request;
                 var response = context.Response;
-
-              
-                string requestUrl = context.Request.RawUrl == "/" ? "index.html" : context.Request.RawUrl.Substring(1);
-                string fullPath = _webRoot + requestUrl;
 
                 try
                 {
-                    if (File.Exists(fullPath))
+                   
+                    if (request.HttpMethod == "POST" && request.RawUrl == "/salvar")
                     {
-                        byte[] buffer = File.ReadAllBytes(fullPath);
-                        response.ContentType = GetMimeType(requestUrl);
-                        response.ContentLength64 = buffer.Length;
-                        response.OutputStream.Write(buffer, 0, buffer.Length);
+                        HandleSalvar(request, response);
                     }
                     else
                     {
-                        response.StatusCode = 404;
+                        HandleStaticFiles(request, response);
                     }
                 }
                 catch
@@ -52,6 +47,55 @@ namespace AvisosSenai
                 {
                     response.Close();
                 }
+            }
+        }
+
+        private void HandleSalvar(HttpListenerRequest request, HttpListenerResponse response)
+        {
+            using (var reader = new StreamReader(request.InputStream))
+            {
+                string body = reader.ReadToEnd();
+
+             
+                string jsonFinal;
+
+                if (File.Exists(_dbPath))
+                {
+                    string existente = File.ReadAllText(_dbPath);
+
+                    if (existente.Length > 2) 
+                        jsonFinal = existente.TrimEnd(']') + "," + body + "]";
+                    else
+                        jsonFinal = "[" + body + "]";
+                }
+                else
+                {
+                    jsonFinal = "[" + body + "]";
+                }
+
+                File.WriteAllText(_dbPath, jsonFinal);
+            }
+
+            response.StatusCode = 200;
+            byte[] ok = Encoding.UTF8.GetBytes("{\"status\":\"ok\"}");
+            response.OutputStream.Write(ok, 0, ok.Length);
+        }
+
+        private void HandleStaticFiles(HttpListenerRequest request, HttpListenerResponse response)
+        {
+            string requestUrl = request.RawUrl == "/" ? "index.html" : request.RawUrl.Substring(1);
+            string fullPath = _webRoot + requestUrl;
+
+            if (File.Exists(fullPath))
+            {
+                byte[] buffer = File.ReadAllBytes(fullPath);
+                response.ContentType = GetMimeType(requestUrl);
+                response.ContentLength64 = buffer.Length;
+                response.OutputStream.Write(buffer, 0, buffer.Length);
+            }
+            else
+            {
+                response.StatusCode = 404;
             }
         }
 
